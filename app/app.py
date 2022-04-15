3184d4fbe58653bcdf52bf31865471f24311dfcd
Jade Qiu <jiayu98@gmail.com>
2022-04-15 03:34:22 -0400

    Updated with code for port and VM

--- app/app.py
+++ app/app.py
@@ -289,5 +289,16 @@ def quiz():
     
 
 if __name__ == "__main__":
+  import click
   app.secret_key = os.urandom(12)
-  app.run(debug=True,host='0.0.0.0', port=4000)
+  @click.command()
+  @click.option('--debug', is_flag=True)
+  @click.option('--threaded', is_flag=True)
+  @click.argument('HOST', default='0.0.0.0')
+  @click.argument('PORT', default=8111, type=int)
+  def run(debug, threaded, host, port):
+    HOST, PORT = host, port
+    print ("running on %s:%d" % (HOST, PORT))
+    app.run(host=HOST, port=PORT, debug=debug, threaded=threaded)
+
+  run()
